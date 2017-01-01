c45b9f95993b24049e67f3b3193a3e339a2f9c5f
mhr380 <hajimemihara1108@gmail.com>
2017-01-01 13:06:17 +0900

    admit PEP8 coding rule

--- TCGAExpDistVisualizer.py
+++ TCGAExpDistVisualizer.py
@@ -3,9 +3,9 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
 
-def load(genomic_matrix,clinical_data):
+
+def load(genomic_matrix, clinical_data):
     try:
         _gdata = pd.read_csv(genomic_matrix, delimiter='\t', index_col=0)
         _cdata = pd.read_csv(clinical_data, delimiter='\t', index_col=0)
@@ -16,41 +16,45 @@ def load(genomic_matrix,clinical_data):
     cdata = _cdata['sample_type']
     return gdata, cdata
 
-def aligndata(gdata,cdata,genename):
+
+def aligndata(gdata, cdata, genename):
     picked_gdata = gdata[genename]
     eval_df = pd.concat([cdata, picked_gdata], axis=1, join='inner')
     return eval_df
+
 
 def show_or_save_chart(eval_df, genename):
 
     bp = eval_df.boxplot(column=genename, by='sample_type')
     bp.plot()
 
-    savename = genename + '.png' 
+    savename = genename + '.png'
 
     plt.savefig(savename)
     print('Saved as' + savename)
 
-    return 
+    return
+
 
 def main(gdata, cdata):
     genelist = list(gdata.T.index)
-    
+
     print('Please enter your gene of interest.')
     genename = input('>>')
-    
-    if not genename in genelist:
+
+    if genename not in genelist:
         print('your GOI is not found')
         return
-    
+
     eval_df = aligndata(gdata, cdata, genename)
     show_or_save_chart(eval_df, genename)
 
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     endflg = False
 
     print("Loading...")
-    gdata, cdata = load("_genomicMatrix","clinical_data")
+    gdata, cdata = load("genomicMatrix", "clinical_data")
 
     while not endflg:
         main(gdata, cdata)
